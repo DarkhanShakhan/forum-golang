dbee65abbfec98bf77fd54cd8b437dca025ac376
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2022-12-11 01:35:28 +0600

    adds crud repo

--- forum_auth/internal/usecase/auth.go
+++ forum_auth/internal/usecase/auth.go
@@ -1,1 +1,26 @@
 package auth_usecase
+
+import (
+	"context"
+	"forum_auth/internal/entity"
+	"log"
+)
+
+type AuthUsecase struct {
+	sessionRepo SessionsRepo
+	errLog      *log.Logger
+}
+
+func NewAuthUsecase(sessionRepo SessionsRepo, errLog *log.Logger) AuthUsecase {
+	return AuthUsecase{sessionRepo: sessionRepo, errLog: errLog}
+}
+
+func (au *AuthUsecase) SignIn(ctx context.Context) {}
+
+func (au *AuthUsecase) SignUp() {}
+
+func (au *AuthUsecase) Authenticate() {}
+
+func (au *AuthUsecase) SignOut(ctx context.Context, session entity.Session, err chan error) {
+	err <- au.sessionRepo.Delete(ctx, session)
+}
---
+++ forum_auth/internal/usecase/interface.go
@@ -0,0 +1,13 @@
+package auth_usecase
+
+import (
+	"context"
+	"forum_auth/internal/entity"
+)
+
+type SessionsRepo interface {
+	Fetch(ctx context.Context, token string) (entity.Session, error)
+	Store(ctx context.Context, session entity.Session) error
+	Update(ctx context.Context, session entity.Session) error
+	Delete(ctx context.Context, session entity.Session) error
+}
