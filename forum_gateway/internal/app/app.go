c4483005ed5e66e6ce863197425410d4eec8acfd
DarkhanShakhan <darkhan.shakhanov@gmail.com>
2022-12-16 18:32:15 +0600

    adds middleware

--- forum_gateway/internal/app/app.go
+++ forum_gateway/internal/app/app.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,20 +8,14 @@ import (
 func Run() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/sign_in", f)
-	mux.HandleFunc("/signin_google", SignInGoogleHandler)
+	mux.Handle("/signin_google", Authenticate(http.HandlerFunc(SignInGoogleHandler)))
 	mux.HandleFunc("/google_callback", GoogleCallbackHandler)
 	// handler := Authenticate(mux)
 	srv := &http.Server{
-		Addr: "localhost:8082",
-		// Handler: handler,
+		Addr:    "localhost:8082",
 		Handler: mux,
 	}
 
 	err := srv.ListenAndServe()
 	log.Fatal(err)
-}
-
-func f(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("here")
 }
--- forum_gateway/internal/app/middleware.go
+++ forum_gateway/internal/app/middleware.go
@@ -2,73 +2,57 @@ package app
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"runtime/debug"
 )
-
-func PanicRecovery(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-				log.Println(string(debug.Stack()))
-			}
-		}()
-		next.ServeHTTP(w, req)
-	})
-}
 
 func Authenticate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		requestUrl := "http://localhost:8081/authenticate"
 		cookier, err := r.Cookie("token")
 		if err != nil {
-			fmt.Println(err)
+			ctx := context.WithValue(r.Context(), "authorised", false)
+			next.ServeHTTP(w, r.WithContext(ctx))
 			return
 		}
 		jsonStr := []byte(fmt.Sprintf(`{"token":"%s"}`, cookier.Value))
 		req, err := http.NewRequest("GET", requestUrl, bytes.NewBuffer(jsonStr))
-
+		if err != nil {
+			ctx := context.WithValue(r.Context(), "authorised", false)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
 		client := &http.Client{}
 		resp, err := client.Do(req)
-		fmt.Println(err)
+		if err != nil {
+			ctx := context.WithValue(r.Context(), "authorised", false)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
 		var authStatus AuthStatusResult
-		json.NewDecoder(resp.Body).Decode(&authStatus) // error
+		if err = json.NewDecoder(resp.Body).Decode(&authStatus); err != nil {
+			ctx := context.WithValue(r.Context(), "authorised", false)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+		if authStatus.Status == NonAuthorised {
+			ctx := context.WithValue(r.Context(), "authorised", false)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
 		cookie := http.Cookie{
 			Name:  "token",
 			Value: authStatus.Token,
 		}
 		http.SetCookie(w, &cookie)
-		fmt.Println(authStatus)
-	})
-	// return func(w http.ResponseWriter, r *http.Request) {
-	// 	requestUrl := "http://localhost:8081/auth"
-	// 	cookie, err := r.Cookie("token")
-	// 	rr := r.Cookies()
-	// 	fmt.Println(rr)
-	// 	fmt.Println(err)
-	// 	if err != nil {
-	// 		return
-	// 	}
-	// 	// FIXME: deal err
-	// 	jsonStr := []byte(fmt.Sprintf(`{"token":"%s"}`, cookie.Value))
-	// 	req, err := http.NewRequest("GET", requestUrl, bytes.NewBuffer(jsonStr))
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		return
-	// 	}
-	// 	req.Header.Set("Content-Type", "application/json")
 
-	// 	client := &http.Client{}
-	// 	resp, _ := client.Do(req)
-	// 	// defer resp.Body.Close()
-	// 	var authStatus AuthStatusResult
-	// 	json.NewDecoder(resp.Body).Decode(&authStatus) // error
-	// 	fmt.Println(authStatus)
-	// }
+		//FIXME: validate data
+		ctx := context.WithValue(context.WithValue(r.Context(), "authorised", true), "user_id", authStatus.UserId)
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 type AuthStatus int
@@ -80,6 +64,7 @@ const (
 
 type AuthStatusResult struct {
 	Status AuthStatus `json:"status,omitempty"`
+	UserId int64      `json:"user_id,omitempty"`
 	Token  string     `json:"token,omitempty"`
 	Err    error      `json:"error,omitempty"`
 }
